1052e335b926af00f16497cae6bffc935b038ba3
mharismazhar-369 <mharis.mazhar@gmail.com>
2026-06-26 06:53:23 -0700

    hero text changed from statrup to services and solutions

--- src/components/marketing/hero.tsx
+++ src/components/marketing/hero.tsx
@@ -49,7 +49,7 @@ export default function Hero() {
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
             <span className="relative inline-flex rounded-full h-3 w-3 bg-primary"></span>
           </span>
-          Start Up Phasse 01
+          Services & Solutions
         </motion.div>
         
         {/* HEADLINE WITH CUSTOM FONT */}
